d2bc6e1f0a939d20c372a5202fd373697172cd9b
01142546 <01142546@K204-63.liwe.ee.pw.edu.pl>
2019-04-03 17:24:11 +0200

    DODANA KLASA PLAYERRRRR

---
+++ src/main/java/Player.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+	List<Bullet> bullets = new ArrayList<>();
+	int x,y;
+	
+	public void strzal()
+	{
+		bullets.add(new Bullet(x,y));
+	}
+	
+	
+	
+}
--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,18 +1,65 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 
-public class Window {
+public class Window implements KeyListener{
 
 	JFrame okno;
 	int size_x=800, size_y=600;
 	
+	
 	Window()
 	{
-		okno=new JFrame("spaceinvider");
+		okno=new JFrame("space invider");
 		okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		okno.setResizable(false);
 		okno.setLocationRelativeTo(null);
 		okno.setSize(size_x, size_y);
 		okno.setVisible(true);
+		okno.addKeyListener(this);
+	}
+	
+	
+	
+	synchronized void draw()
+	{
+		
+	}
+
+	void strzal()
+	{
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		
+		
+		if(arg0.getKeyCode()==KeyEvent.VK_SPACE)
+		{
+			System.out.println("strzal");
+			
+		}
+		
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
