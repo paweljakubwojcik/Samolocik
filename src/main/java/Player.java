1f1aa01ac0f74be858b4c5e2e9af59ffc7667d42
01142498 <01142498@K204-66.liwe.ee.pw.edu.pl>
2019-04-03 17:46:22 +0200

    Add somethink

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Player {
 	List<Bullet> bullets = new ArrayList<>();
-	int x, y;
+	int x = 400, y = 500;
 
 	public void strzal() {
 		bullets.add(new Bullet(x, y));
--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,14 +1,23 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Window implements KeyListener {
 
 	JFrame okno;
 	int size_x = 800, size_y = 600;
+	BufferedImage klatka = new BufferedImage(size_x, size_y, BufferedImage.TYPE_INT_RGB);
+	BufferedImage statek;
+	Player statek1 = new Player();
 
 	Window() {
 		okno = new JFrame("space invider");
@@ -16,11 +25,36 @@ public class Window implements KeyListener {
 		okno.setResizable(false);
 		okno.setLocationRelativeTo(null);
 		okno.setSize(size_x, size_y);
+		okno.setLayout(null);
 		okno.setVisible(true);
 		okno.addKeyListener(this);
+
+		URL url = getClass().getResource("samolot.png");
+		try {
+			statek = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		draw();
+
 	}
 
 	synchronized void draw() {
+		while (true) {
+			Graphics2D g = (Graphics2D) klatka.getGraphics();
+			g.setColor(Color.BLACK);
+			g.drawRect(0, 0, size_x, size_y);
+			g.drawImage(statek, statek1.x, statek1.y, null);
+			g.dispose();
+			drawklatka();
+		}
+	}
+
+	void drawklatka() {
+		Graphics2D g2d = (Graphics2D) okno.getGraphics();
+		g2d.drawImage(klatka, 0, 0, null);
+		g2d.dispose();
 
 	}
 
