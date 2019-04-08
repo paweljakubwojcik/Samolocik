5304698db28ec5869e3470ad9d56de84f8d9bcca
Pafeu <Pafeu@LAPTOP-I0IJ9E63>
2019-04-08 16:43:59 +0200

    Asteroid update

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -3,19 +3,23 @@ import java.awt.Graphics2D;
 
 public class Asteroid extends Enemy implements IEnemy {
 
+	
+	static final int defaultHealth=2;
+	
+	
 	/**
 	 * @param window
 	 * @param x
 	 * @param y
 	 * 
 	 */
-	Asteroid(Window win, int x, int y) {
-		super(x, y);
+	Asteroid(Window win) {
+		super(generator.nextInt(win.size_x), -10);
 		this.win = win;
-		this.health = 100;
+		this.health = defaultHealth;
 		width = generator.nextInt(70) + 30;
 		height = generator.nextInt(70) + 30;
-		velocity_x = generator.nextInt(5) + 1;
+		velocity_x = generator.nextInt(2) + 1;
 		velocity_y = generator.nextInt(2) + 1;
 		zakresRuchu = x + generator.nextInt(win.size_x - x);
 	}
@@ -24,15 +28,19 @@ public class Asteroid extends Enemy implements IEnemy {
 	public void drawMe(Graphics2D g) {
 		g.setColor(Color.orange);
 		g.fillOval(x, y, width, height);
+		
+		g.setColor(Color.red);
+		g.drawRect(x, y+win.size_y/400, win.size_x/15, win.size_y/300);
+		g.fillRect(x, y+win.size_y/400, (win.size_x/15)*health/defaultHealth, win.size_y/300 );
+		
 	}
 
 	@Override
 	public void myMotion() {
 
-		x += velocity_x;
 		y += velocity_y;
 
-		if (x > zakresRuchu && velocity_x > 0) {
+		/*if (x > zakresRuchu && velocity_x > 0) {
 			velocity_x = -velocity_x;
 			zakresRuchu = win.size_x - generator.nextInt(x);
 		}
@@ -40,7 +48,7 @@ public class Asteroid extends Enemy implements IEnemy {
 		if (x < zakresRuchu && velocity_x < 0) {
 			velocity_x = -velocity_x;
 			zakresRuchu = x + generator.nextInt(win.size_x - x);
-		}
+		}*/
 
 	}
 }
