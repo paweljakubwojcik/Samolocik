b7e851dd94291a8651bc48f20a65ef076a68524d
7Adrian <7adrian.mail@gmail.com>
2019-04-08 15:20:25 +0200

    Paszko2

--- src/main/java/BossPaszko.java
+++ src/main/java/BossPaszko.java
@@ -1,12 +1,26 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class BossPaszko extends Enemy implements IEnemy {
 
-	BufferedImage statek;
+	BufferedImage paszko;
 	
-	BossPaszko(int x, int y) {
+	BossPaszko(Window win, int x, int y) {
 		super(x, y);
+		this.win = win;
+		this.x = x;
+		this.y = y;
+
+		URL url = getClass().getResource("samolot.png");
+		try {
+			paszko = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
