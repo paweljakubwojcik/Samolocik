0b48d880c33ec22d8a2025addf356550f2b3af67
01142498 <01142498@K204-66.liwe.ee.pw.edu.pl>
2019-05-08 17:13:24 +0200

    Apteczka

--- src/main/java/HealthPack.java
+++ src/main/java/HealthPack.java
@@ -5,46 +5,44 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 
-public class HealthPack extends Drop{
-	
-	static BufferedImage apteczka; 
+public class HealthPack extends Drop {
+
+	static BufferedImage apteczka;
 
 	HealthPack(int x, int y) {
 		super(x, y);
-		this.width=50;
-		this.height=50;
-		System.out.print("hey");
+		this.width = 50;
+		this.height = 50;
 		try {
-			URL url = getClass().getResource("apteczka.png");
+			URL url = getClass().getResource("images//apteczka.png");
 			apteczka = ImageIO.read(url);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 	@Override
 	void drawMe(Graphics2D g) {
 		g.drawImage(apteczka, x, y, null);
-		
+
 	}
 
 	@Override
 	public int[][] getPole() {
-		int[][] tab = {{x,y,width,height}};
+		int[][] tab = { { x, y, width, height } };
 		return tab;
 	}
 
 	@Override
 	void collision(Object o) {
-		if(o.getClass()==Player.class)
-		{
+		if (o.getClass() == Player.class) {
 			Player player = (Player) o;
-			player.health+=30;
-			if(player.health>player.DefaultHealth) player.health=player.DefaultHealth;
+			player.health += 30;
+			if (player.health > player.DefaultHealth)
+				player.health = player.DefaultHealth;
 			drops.remove(this);
 		}
-		
+
 	}
 
 }
