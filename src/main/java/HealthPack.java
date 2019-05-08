49f227563a2a0d22da5b9d0e7f944e0470ede5b7
01142498 <01142498@K204-66.liwe.ee.pw.edu.pl>
2019-05-08 16:49:32 +0200

    apteczka

--- src/main/java/HealthPack.java
+++ src/main/java/HealthPack.java
@@ -1,20 +1,31 @@
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class HealthPack extends Drop{
+	
+	static BufferedImage apteczka; 
 
 	HealthPack(int x, int y) {
 		super(x, y);
 		this.width=50;
 		this.height=50;
 		System.out.print("hey");
+		try {
+			URL url = getClass().getResource("apteczka.png");
+			apteczka = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	void drawMe(Graphics2D g) {
-		g.setColor(Color.magenta);
-		g.fillRect(x, y, width, height);
+		g.drawImage(apteczka, x, y, null);
 		
 	}
 
